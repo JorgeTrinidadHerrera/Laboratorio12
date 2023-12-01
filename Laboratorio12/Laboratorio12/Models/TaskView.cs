3bc87df75d3fa6acb973c82c2060b95510e345b5
Alumno <Alumno@LAB1509-09>
2023-12-01 14:19:07 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio12/Laboratorio12/Models/TaskView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Laboratorio12.Models
+{
+    public class TaskView : ContentPage
+    {
+        public TaskView()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
